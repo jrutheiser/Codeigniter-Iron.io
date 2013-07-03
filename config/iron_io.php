fdd98aa4440325b3782ced2dec40cbef07e9dbe3
Jonathan Rutheiser <jonathan.rutheiser@gmail.com>
2013-07-02 22:22:00 -0700

    fixed config

---
+++ config/iron_io.php
@@ -0,0 +1,31 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ *
+ *  Iron.io Configurations
+ * 
+ */
+
+/**
+ * Iron.io Credentials
+ */
+$config['auth_token'] = '';
+$config['project_id'] = '';
+
+/**
+ * API Configurations
+ */
+$config['protocol'] = 'https';
+
+$config['host']     =  array(
+    'IronMQ'     => 'mq-aws-us-east-1.iron.io', // for Rackspace use mq-rackspace-dfw.iron.io
+    'IronWorker' => 'worker-aws-us-east-1.iron.io',
+    'IronCache'  => 'cache-aws-us-east-1.iron.io'
+);
+
+$config['port']     = '443';
+
+
+/**
+ * Cache Name for IronCache
+ */
+$config['cache_name'] = 'test';
